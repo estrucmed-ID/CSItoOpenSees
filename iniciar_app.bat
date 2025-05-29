72a9e7b0930ea8ca712ea9496249dc6b0e899f50
estrucmed-ID <estrucmed@gmail.com>
2025-05-29 09:30:07 -0500

    Ajuste de inicio de plataforma con inicial_app.bat

--- iniciar_app.bat
+++ iniciar_app.bat
@@ -1,8 +1,29 @@
 @echo off
-echo Activando entorno virtual...
+setlocal
+
+echo ================================
+echo Iniciando la plataforma...
+echo ================================
+
+REM Verificar si ya existe el entorno virtual
+if not exist ".venv" (
+    echo [1/4] Creando entorno virtual .venv con Python 3.12...
+    python -m venv .venv
+)
+
+REM Activar el entorno virtual
+echo [2/4] Activando entorno virtual...
 call .venv\Scripts\activate.bat
 
-echo Ejecutando la aplicación...
+REM Instalar dependencias
+echo [3/4] Instalando requerimientos...
+pip install -r docs\requirements.txt > nul
+
+REM Ejecutar la aplicación
+echo [4/4] Ejecutando la plataforma...
 python main.py
 
-pause
+echo.
+echo Presiona cualquier tecla para cerrar...
+pause > nul
+
