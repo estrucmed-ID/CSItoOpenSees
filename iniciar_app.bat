54a878ce94609d58fadf6ee7bfb0e4f7a109df49
estrucmed-ID <estrucmed@gmail.com>
2025-05-29 10:01:13 -0500

    Actualizacion de iniciar_app con instalacion de python

--- iniciar_app.bat
+++ iniciar_app.bat
@@ -1,36 +1,4 @@
 @echo off
-set PYTHON_VERSION=3.12.10
-set INSTALLER=python-%PYTHON_VERSION%-amd64.exe
-set DOWNLOAD_URL=https://www.python.org/ftp/python/%PYTHON_VERSION%/%INSTALLER%
-
-echo ----------------------------------------
-echo [0] Verificando si Python %PYTHON_VERSION% está instalado...
-echo ----------------------------------------
-
-py -%PYTHON_VERSION% --version >nul 2>&1
-if %errorlevel% neq 0 (
-    echo Python %PYTHON_VERSION% no está instalado.
-    echo Descargando instalador desde: %DOWNLOAD_URL%
-
-    curl -o %INSTALLER% %DOWNLOAD_URL%
-    if not exist %INSTALLER% (
-        echo ERROR: No se pudo descargar Python. Verifique su conexión a internet.
-        pause
-        exit /b 1
-    )
-
-    echo Instalando Python %PYTHON_VERSION% silenciosamente...
-    %INSTALLER% /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
-
-    if %errorlevel% neq 0 (
-        echo ERROR: Falló la instalación de Python.
-        pause
-        exit /b 1
-    )
-
-    echo Python instalado correctamente.
-)
-
 echo -------------------------------
 echo [1] Verificando entorno virtual
 echo -------------------------------
